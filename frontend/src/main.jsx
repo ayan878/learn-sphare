35e496fd8427f968a29180ce6b1a53e6f2795e74
ayan878 <ayanraza197@gmail.com>
2024-08-02 18:00:52 +0530

    main

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import { configureStore } from "@reduxjs/toolkit";
+import globalReducer from "./state/globalSlice"
+import { Provider } from "react-redux";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+
+
+
+const store=configureStore({
+  reducer:globalReducer,
+
+})
+
+const theme = createTheme({
+  // You can customize the theme here
+});
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <ThemeProvider theme={theme}>
+    <CssBaseline />
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </ThemeProvider>
+);
